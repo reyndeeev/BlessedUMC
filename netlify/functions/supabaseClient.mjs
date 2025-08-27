9644898f5a4d5f921c0c4da3a8f947a67e555e5b
reyndeeev <danielaustriaqt@gmail.com>
2025-08-28 00:36:55 +0800

    qweqe

--- netlify/functions/supabaseClient.mjs
+++ netlify/functions/supabaseClient.mjs
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 
 // Replace these with your actual Supabase project URL and service_role key
 const SUPABASE_URL = process.env.SUPABASE_URL || 'https://unkaptrjqaveyfqeildi.supabase.co';
-const SUPABASE_SERVICE_KEY = process.env.SUPABASE_SERVICE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVua2FwdHJqcWF2ZXlmcWVpbGRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYzMDc3ODksImV4cCI6MjA3MTg4Mzc4OX0.Acz-djFigpU9ASrfXDx2nFAgyWAuSve_uUVK9T19-pY';
+const SUPABASE_SERVICE_KEY = process.env.SUPABASE_SERVICE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVua2FwdHJqcWF2ZXlmcWVpbGRpIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1NjMwNzc4OSwiZXhwIjoyMDcxODgzNzg5fQ.l7Fu_X9iT4AH6uLzlaKM1Rhdnkm5MXSc9CVeejButxY';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_KEY);
  
